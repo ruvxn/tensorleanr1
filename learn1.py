71f66682eb2ee773b4459b5545bd283bd25d3150
Ruveen Jayasinghe <ruveenjay@gmail.com>
2025-02-01 12:00:56 +1100

    Update learn1.py

--- learn1.py
+++ learn1.py
@@ -19,3 +19,21 @@ plt.title("Celsius to Fahrenheit Conversion")
 
 plt.show()
 
+#training the model
+
+x_train = temp_df['Celsius']
+y_train = temp_df['Fahrenheit']
+
+print(x_train.shape)
+print(y_train.shape)
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(1, input_shape=[1]))
+
+print(model.summary())
+
+#compile model
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1.0),loss='mean_squared_error')
+
+epochs_hist = model.fit(x_train, y_train, epochs=100)
+
